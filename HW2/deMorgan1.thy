63635622c93181c31a503256873cc62a4cf394c2
dzhou1337 <dzhou1337@gmail.com>
2021-09-13 10:59:19 -0400

    deMorgan1

---
+++ HW2/deMorgan1.thy
@@ -0,0 +1,18 @@
+theory deMorgan1
+  imports Main
+begin
+  text\<open> Apply style \<close>
+   lemma lem_k_1 : "\<not>(p \<or> q) \<longrightarrow> (\<not>p \<and> \<not>q)"
+    apply (rule impI)
+    apply (rule conjI)
+    apply (rule notI)
+    apply (erule notE)
+     apply(rule disjI1)
+       apply assumption
+      apply (rule notI)
+      apply (erule notE)
+      apply (rule disjI2)
+     apply assumption
+    done
+
+end
